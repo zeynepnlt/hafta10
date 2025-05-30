00beaac67e90380e810cb93e31ff0293595311ca
zeynepnlt <zsudenalbant@stu.aydin.edu.tr>
2025-05-30 11:08:05 +0300

    Add files via upload

--- exception.py
+++ exception.py
@@ -3,16 +3,11 @@ import sys
 def uret(str):
     a=''
     b=0
-    s2 = sys.argv[2]
-    s3 = sys.argv[3]
-    k = s2.split('=')
-    print(k)
-    a=k[1]
-
-    l = s3.split('=')
-    b=l[1]
+    k = str[2]
+    l = str[3]
+    a = k.split('=')[1]
+    b = l.split('=')[1]
     return a,b
-
 
 zeynep=sys.argv
 x,y=uret(zeynep)
